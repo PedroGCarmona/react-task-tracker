bd55934f510cab69969f5384228c18c4fc9dfcf4
Pedro Gabriel Carmona <pgcarmona@outlook.com>
2022-06-18 05:04:35 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import AddTask from './components/AddTask';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [showAddTask, setShowAddTask] = useState(false)
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask ? <AddTask onAdd={addTask} /> : <></>}
+      <Tasks tasks={tasks} deleteTask={deleteTask} toggleReminder={toggleReminder} />
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,13 @@
+import Task from './Task'
+
+const Tasks = ({ tasks, deleteTask, toggleReminder }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task task={task} deleteTask={deleteTask} toggleReminder={toggleReminder} />
+      ))}
+    </>
+  )
+}
+
+export default Tasks
